38b1f1e1a97fae9b7b5b3415be08cba98146f740
KimSungJun <jt2302@naver.com>
2026-03-10 20:57:31 +0900

    feat: p10171 success

---
+++ src/step01/p10171/Main.java
@@ -0,0 +1,16 @@
+package step01.p10171;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("\\    /\\" + "\n");
+        bw.write(" )  ( ')" + "\n");
+        bw.write("(  /  )" + "\n");
+        bw.write(" \\(__)|");
+        bw.flush();
+        bw.close();
+    }
+}

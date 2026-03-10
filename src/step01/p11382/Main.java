1587f5d37c7ab1f66d5193fadf5f31276c7403e0
KimSungJun <jt2302@naver.com>
2026-03-10 20:45:39 +0900

    feat: p11382 success

---
+++ src/step01/p11382/Main.java
@@ -0,0 +1,22 @@
+package step01.p11382;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        long c = Long.parseLong(st.nextToken());
+
+        long result = a + b + c;
+
+        bw.write(String.valueOf(result));
+        bw.flush();
+        bw.close();
+    }
+}

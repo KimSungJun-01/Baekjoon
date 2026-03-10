8aaca80cd5014454848316c9bb79f1c1914d6c65
KimSungJun <jt2302@naver.com>
2026-03-10 10:47:12 +0900

    feat: p10869 success

--- src/step01/p10869/Main.java
+++ src/step01/p10869/Main.java
@@ -1,4 +1,25 @@
 package step01.p10869;
 
+import java.io.*;
+import java.util.StringTokenizer;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        int sum = a + b;
+        int sub = a - b;
+        int mul = a * b;
+        int div = a / b;
+        int mod = a % b;
+
+        bw.write(String.valueOf(sum + "\n" + sub + "\n" + mul + "\n" + div + "\n" + mod));
+        bw.flush();
+        bw.close();
+    }
 }

baed6e874cc94814ce143fe0185ad9ffb0ff02f4
KimSungJun <jt2302@naver.com>
2026-03-10 10:42:14 +0900

    feat: p1008 success

--- src/step01/p10869/Main.java
+++ src/step01/p10869/Main.java
@@ -1,21 +1,4 @@
 package step01.p10869;
 
-import java.io.*;
-import java.util.StringTokenizer;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
-
-        double result = a / (double) b;
-
-        bw.write(String.valueOf(result));
-        bw.flush();
-        bw.close();
-    }
 }

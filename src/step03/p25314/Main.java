adeb038ff3e1257516a672f842893982d5a04c80
KimSungJun <jt2302@naver.com>
2026-03-17 17:37:05 +0900

    feat: p25314 success

---
+++ src/step03/p25314/Main.java
@@ -0,0 +1,20 @@
+package step03.p25314;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int count = n / 4;
+
+        for(int i = 0; i < count; i++) {
+            bw.write("long ");
+        }
+        bw.write("int");
+        bw.flush();
+        bw.close();
+    }
+}

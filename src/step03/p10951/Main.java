7d365c76d3cd7f46121d3d87e68f095f5fc15e06
KimSungJun <jt2302@naver.com>
2026-03-17 20:36:38 +0900

    feat: p10951 success

---
+++ src/step03/p10951/Main.java
@@ -0,0 +1,24 @@
+package step03.p10951;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        String line = "";
+
+        while((line = br.readLine()) != null) {
+            st = new StringTokenizer(line);
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            bw.write(String.valueOf((a + b) + "\n"));
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
